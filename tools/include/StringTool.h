c771477cfe469f8a73ddce861978a1580bf75982
alexis <alexis.bessonnat@epitech.eu>
2024-12-15 21:08:05 +0100

    Ajout d'une fonction de vérification

--- tools/include/StringTool.h
+++ tools/include/StringTool.h
@@ -1,6 +1,8 @@
 #ifndef STRING_TOOLS
 #define STRING_TOOLS
 
+#include "string.h"
+
 int countCharInString(const char* s, const int length, const char c);
 
 char isCharExistInArray(const char c, const char* array, const int length);
@@ -14,5 +16,7 @@ char* copyString(char* ori);
 char isIdenticalStr(const char * str1, const char * str2);
 
 char isIdenticalStr_misc(const void * str1, const void * str2);
+
+char isNumeric(const char* str1);
 #endif
 
